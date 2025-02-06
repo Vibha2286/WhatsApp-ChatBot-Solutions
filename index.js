1d02dae6f5acb7029e49c5ec2399dcb807d2a68b
Vibha <vibha.pawar@rediffmail.com>
2025-02-06 09:16:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const axios=require("axios")
+require('dotenv').config()
+
+const app=express().use(bodyparser.json());
+
+const token=process.env.TOKEN
+const mytoken=process.env.MYTOKEN
+
+app.listen(8000||process.env.PORT,()=>{
+    console.log("webhook is listening");
+})
+
+app.get("webhook",(request, response)=>{
+    let mode=request.query["hub.mode"]
+    let challenge=request.query["hub.challenge"]
+    let token=request.query["hub.verify_token"]
+
+    if(mode==="subscrib(e" && token===mytoken){
+        response.status(200).send(challenge)
+    } else {
+        response.status(403)
+    }
+})
+
+
+app.post("/webhook",(request, response)=>{
+    let body_para=request.body
+    console.log(JSON.stringify(body_para,null,2))
+
+    if(body_para.object){
+        if(body_para.entry && 
+           body_para.entry[0].changes[0] &&
+           body_para.entry[0].changes[0].value.messages && 
+           body_para.entry[0].changes[0].value.messages[0]) {
+            
+            let phone_no_id=body_para.entry[0].changes[0].value.metadata.phone_number_id
+            let from=body_para.entry[0].changes[0].value.messages[0].from
+            let msg_body=body_para.entry[0].changes[0].value.messages[0].text.body
+
+           axios ({
+            method:"POST",
+            url:"https://graph.facebook.com/v21.0/"+phone_no_id+"/messages?access_token?"+token,
+            data:{
+                messaging_product:"WhatsApp",
+                to:from,
+                text:{
+                    body:"Hi, Welcome to Vibha's Chatbot Solution"
+                }
+            },
+            headers:{
+                "Content-Type":"application/json"
+            }
+           })
+           response.sendStatus(200)
+        } else {
+            response.sendStatus(404)
+        }
+    }
+})
